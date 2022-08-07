4f5287aac0bf0aa41e47371001b3769fcb821ada
Juhyun-Bak <45312398+Juhyun22@users.noreply.github.com>
2022-08-08 00:15:11 +0900

    Create tuple.py

---
+++ practice/tuple.py
@@ -0,0 +1,14 @@
+# 리스트인데 변경 불가. 대신, 속도 빠름
+menu = ("돈까스", "치즈까스")
+print(menu[0])
+print(menu[1])
+
+# menu.add("생선까스") 불가ㅎㅎ
+
+# name = "김종국"
+# age = 20
+# hobby = "코딩"
+# print(name, age, hobby)
+
+(name, age, hobby) = ("김종국", 20, "코딩")
+print(name, age, hobby)
