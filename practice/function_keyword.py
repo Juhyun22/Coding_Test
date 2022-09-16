ec3cfc5452e5f12ddf4ddb7ed32f940436d478d9
Juhyun-Bak <45312398+Juhyun22@users.noreply.github.com>
2022-09-16 18:50:07 +0900

    Create function_keyword.py

---
+++ practice/function_keyword.py
@@ -0,0 +1,9 @@
+# function_keyword
+
+# 예제 1
+def profile(name, age, main_lang):
+    print(name, age, main_lang)
+    
+profile(name = "유재석", main_lang = "파이썬", age = 20)
+profile(main_lang = "자바", age = 25, name = "김태호")
+
