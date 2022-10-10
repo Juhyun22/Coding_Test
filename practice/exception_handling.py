18bef0886467fa7b042d497819af74691ec19428
Juhyun-Bak <45312398+Juhyun22@users.noreply.github.com>
2022-10-11 00:58:45 +0900

    Create exception_handling.py

---
+++ practice/exception_handling.py
@@ -0,0 +1,18 @@
+# 예외 처리
+# exception handling
+
+# 예제 1
+try:
+    print("나누기 전용 계산기입니다.")
+    nums = []
+    nums.append(int(input("첫번째 숫자를 입력하세요 : ")))
+    nums.append(int(input("두번째 숫자를 입력하세요 : ")))
+    # nums.append(int(nums[0] / nums[1]))
+    print("{0} / {1} = {2}".format(nums[0], nums[1], int(nums[2])))
+except ValueError:
+    print("에러! 잘못된 값을 입력하셨습니다.")
+except ZeroDivisionError as err:
+    print(err)
+except Exception as err: # 나머지 모든 에러 처리 
+    print("일 수 없는 에러가 발생하였습니다.")
+    print(err)
