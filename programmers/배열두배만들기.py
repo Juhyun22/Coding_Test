9d3b4cb400d80da1afc3f8b8de22eab21231a051
Juhyun-Bak <45312398+Juhyun22@users.noreply.github.com>
2022-10-12 00:49:21 +0900

    Create 배열두배만들기.py

---
+++ programmers/배열두배만들기.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    answer = []
+    for num in numbers:
+        answer.append(num * 2)
+    return answer
+  
+  # 다른 사람 풀이
+  def solution(numbers):
+    return [num * 2 for num in numbers]
