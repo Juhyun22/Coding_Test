cd159e8715eb289078fbd7d167f2285aa6197004
Juhyun-Bak <45312398+Juhyun22@users.noreply.github.com>
2022-10-11 13:28:14 +0900

    Create practicpip.py

---
+++ practicpip.py
@@ -0,0 +1,13 @@
+# pip install
+# pypi = 파이썬 패키지 
+
+# 예제 1
+# beautifulsoup
+from bs4 import BeautifulSoup
+soup = BeautifulSoup("<p>Some<b>bad<i>HTML")
+print(soup.prettify())
+
+# pip list = 설치되어 있는 파일들
+# pip show 파일 이름
+# pip install --upgrade 파일 이름
+# pip uninstall 파일 이름
