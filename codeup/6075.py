c952648023009ee6bd402e4ed1d325b28163f022
Juhyun-Bak <45312398+Juhyun22@users.noreply.github.com>
2022-10-15 09:49:26 +0900

    Create 6075.py
    [기초-반복실행구조] 정수 1개 입력받아 그 수까지 출력하기1

---
+++ codeup/6075.py
@@ -0,0 +1,19 @@
+# 내 풀이
+n = int(input())
+i = 0
+
+while True:
+    if i != n:
+        print(i)
+        i += 1
+    else:
+        print(i)
+        break
+        
+# 다른 사람 풀이
+n = int(input())
+i = 0
+
+while i <= n:
+  print(n)
+  n += 1
