b81c638b02fee8ea41cbbfea69cfbdd409259d8b
Juhyun-Bak <45312398+Juhyun22@users.noreply.github.com>
2022-10-17 00:49:06 +0900

    Create 짝수의합.py

---
+++ programmers/짝수의합.py
@@ -0,0 +1,15 @@
+# 나의 풀이
+def solution(n):
+  answer = 0
+  for i in range(0, n + 1):
+    if i % 2 == 0:
+      answer += i
+return answer
+
+# 다른 사람 풀이 1
+def solution(n):
+  return sum([i for i in range(2, n + 1, 2)])
+
+# 다른 사람 풀이 2
+def solution(n):
+  return sum(i for i in range(0, n + 1) if i % 2 == 0)
