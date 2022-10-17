cca2defb56b855d2f62885f14e4b43ece38839ed
Juhyun-Bak <45312398+Juhyun22@users.noreply.github.com>
2022-10-17 17:08:32 +0900

    Create 6098.py
    [기초-리스트] 성실한 개미

---
+++ codeup/6098.py
@@ -0,0 +1,27 @@
+# 하나 하나 생각해보면 풀 수 있따!!
+array = []
+
+for i in range(10):
+    array.append(list(map(int, input().split())))
+    
+x, y = 1, 1
+
+while True:
+    if (array[x][y] == 0):
+        array[x][y] = 9
+    elif (array[x][y] == 2):
+        array[x][y] = 9
+        break 
+    
+    if ((array[x][y + 1] == 1) and (array[x + 1][y] == 1)):
+        break 
+    
+    if (array[x][y + 1] != 1):
+        y += 1
+    elif (array[x + 1][y] != 1):
+        x += 1 
+
+for i in range(10):
+    for j in range(10):
+        print(array[i][j], end=' ')
+    print()
