390ce800aaa03c2bb072b72142b0cb64e88d4e52
Juhyun-Bak <45312398+Juhyun22@users.noreply.github.com>
2022-10-22 23:03:14 +0900

    Create 홀수와짝수.py

---
+++ programmers/홀수와짝수.py
@@ -0,0 +1,13 @@
+# 나의 풀이
+def solution(num):
+  answer = ''
+  if num % 2 == 0:
+    answer = 'Even'
+  else:
+    answer = 'Odd'
+  return answer
+
+# 다른 사람 풀이
+def solution(num):
+  return "Even" if num % 2 == 0 else "Odd"
+  
