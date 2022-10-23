71a34d16bf165eb0b51403be5bde43f76cbba62e
Juhyun-Bak <45312398+Juhyun22@users.noreply.github.com>
2022-10-23 23:18:38 +0900

    Create 나누어떨어지는숫자배열.py

---
+++ programmers/나누어떨어지는숫자배열.py
@@ -0,0 +1,14 @@
+# 나의 풀이
+def solution(arr, divisor):
+  answer = []
+  for i in arr:
+    if i % divisor == 0:
+      answer.append(i)
+  if len(answer) == 0:
+    answer.append(-1)
+  answer.sort()
+  return answer
+
+# 다른 사람 풀이
+def solution(arr, divisor):
+  return sorted([n for n in arr if n % divisor == 0]) or [-1]
