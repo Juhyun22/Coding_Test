1803afc38a6b21e1902e470ba8459fc2b2e0af2d
Juhyun-Bak <45312398+Juhyun22@users.noreply.github.com>
2022-10-24 15:18:15 +0900

    Update 3월에태어난여성회원목록출력하기.sql

--- programmers/SQL/SELECT/3월에태어난여성회원목록출력하기.sql
+++ programmers/SQL/SELECT/3월에태어난여성회원목록출력하기.sql
@@ -1,5 +1,10 @@
-SELECT MEMBER_ID, MEMBER_NAME, GENDER, DATE_OF_BIRTH
+SELECT MEMBER_ID, MEMBER_NAME, GENDER, date_format(DATE_OF_BIRTH, '%Y-%m-%d')
 FROM MEMBER_PROFILE
-WHERE MONTH(DATE_OF_BIRTH) = '3' and not TLNO is null
+WHERE MONTH(DATE_OF_BIRTH) = 3
+and GENDER = 'W' 
+and not TLNO is null
+order by MEMBER_ID;
 
 # null인지 아닌지 -> is null / not 뭐뭐 is null
+# date 출력 이쁘게 -> date_format
+# date 달만 뽑아내기 -> month(date)
