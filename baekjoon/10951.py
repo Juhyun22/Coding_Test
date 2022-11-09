218be2c2bb068ab36439b78ebc32bdf131e625a2
Juhyun-Bak <45312398+Juhyun22@users.noreply.github.com>
2022-11-09 19:13:26 +0900

    Create 10951.py

---
+++ baekjoon/10951.py
@@ -0,0 +1,14 @@
+# 다른 사람 풀이
+while True:
+  try:
+    a, b = map(int, input().split())
+    print(a + b)
+  except:
+    break
+    
+try:
+  while True:
+    a, b = map(int, input().split())
+    print(a + b)
+except:
+  exit()
